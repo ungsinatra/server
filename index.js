2400c11431c433478bce074a6a0625cb81779cae
ungsinatra <mamirov.21@mail.ru>
2023-04-17 20:53:56 +0300

    FIX

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use('/api/answers', userAnswerRouter);
 app.post('/api/singin', login);
 app.post('/api/singup', createUserController);
 app.use(cors({
-  origin: ["http://localhost:3001/",'http://localhost:3000/'],
+  origin: ["http://localhost:3001",'http://localhost:3000'],
   methods:['GET','POST',"PUT","UPDATE","PATCH"]
 }));
 const startApp = async () => {
